1414f44a8134ab961792baefc95d530f52dd4792
Tural Gasimov <130240067+turalgasimov@users.noreply.github.com>
2025-02-25 20:19:13 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install espeak-ng
-apt-get update && apt-get install -y espeak-ng
+sudo apt-get update && apt-get install -y espeak-ng
 
 # Install Python dependencies
 pip install -r requirements.txt
